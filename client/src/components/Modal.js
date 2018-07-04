d35ebcc771d3dc5d6cb3fe3c8becfab27574d0ac
mattcarpowich1 <mattcarpowich@gmail.com>
2018-07-03 17:28:44 -0700

    new run entry form basics

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,18 +1,30 @@
 import React from 'react'
 
 const Modal = props => (
-  <div
-    style={modalStyle}
-    className='modal'>
-    <p
-      style={xStyle}
-      className='modal-close-x'
-      onClick={props.handleClose}>
-      x
-    </p>
-    {props.children}
+  <div style={outerModalStyle}>
+    <div
+      style={modalStyle}
+      className='modal'>
+      <p
+        style={xStyle}
+        className='modal-close-x'
+        onClick={props.handleClose}>
+        x
+      </p>
+      {props.children}
+    </div>
   </div>
 )
+
+const outerModalStyle = {
+  zIndex: 5,
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  height: '100vh',
+  width: '100vw',
+  background: 'var(--black-fade)'
+}
 
 const modalStyle = {
   zIndex: 10,
@@ -21,7 +33,8 @@ const modalStyle = {
   left: '5vw',
   height: '90vh',
   width: '90vw',
-  background: 'var(--main-gold)'
+  background: 'var(--main-gold)',
+  overflowY: 'scroll'
 }
 
 const xStyle = {
@@ -31,7 +44,7 @@ const xStyle = {
   top: '16px',
   right: '16px',
   fontSize: '20px',
-  color: 'var(--main-purple)'
+  color: 'var(--black)'
 }
 
 export default Modal
--- client/src/components/NewRunEntry.js
+++ client/src/components/NewRunEntry.js
@@ -4,21 +4,67 @@ import moment from 'moment'
 const NewRunEntry = ({runDate}) => {
   const formattedDate = moment(runDate).format('MMM Do YY')
   return (
-    <form style={formStyle}>
+    <form style={formStyle} className='new-run-form'>
       <h3 style={h3Style}>
         {formattedDate}
       </h3>
+      <div className='form-field'>
+        <label>DISTANCE</label>
+        <div className='distance-field'>
+          <input name='distance' type='text' size={3} />
+          <p>mi</p>
+        </div>
+      </div>
+      <div className='form-field'>
+        <label>DURATION</label>
+        <div className='duration-field'>
+          <input name='hours' type='text' size={3} />
+          <p>hr</p>
+          <input name='minutes' type='text' size={3} />
+          <p>min</p>
+          <input name='seconds' type='text' size={3} />
+          <p>s</p>
+        </div>
+      </div>
+      <div className='form-field'>
+        <label>COMMENT</label>
+        <div className='comment-field'>
+          <textarea name='comment' rows={8}/>
+        </div>
+      </div>
+      <div className='form-field'>
+        <button type='submit' style={buttonStyle}>
+          SAVE
+        </button>
+      </div>
     </form>
   )
 }
 
 const formStyle = {
-  margin: '24px'
+  margin: '24px',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-start',
+  alignItems: 'flex-start'
 }
 
 const h3Style = {
   fontFamily: 'var(--font-secondary)',
-  fontSize: 'var(--small-responsive-font)'
+  fontSize: 'var(--small-responsive-font)',
+  color: 'var(--black)',
+  margin: 0,
+  marginBottom: 16
+}
+
+const buttonStyle = {
+  fontFamily: 'var(--font-secondary)',
+  border: 'none',
+  background: 'var(--black)',
+  color: 'var(--white)',
+  fontSize: 'var(--tiny-responsive-font)',
+  padding: '8px 12px',
+  marginTop: '4px'
 }
 
 export default NewRunEntry
