4c522894c5dcbe4f1462c7072b6c198e74f292a1
mattcarpowich1 <mattcarpowich@gmail.com>
2018-07-04 18:40:59 -0700

    separated controllers from routes

--- models/workout.js
+++ models/workout.js
@@ -1,6 +1,24 @@
 const client = require('./connection.js');
 
 module.exports = {
+	findAll: id => {
+		return new Promise((resolve, reject) => {
+			const query = {
+			  text: `SELECT workout_id, workout_type, distance, duration, comment, workout_date 
+			  	FROM workouts WHERE workout_user_id=$1`,
+			  values: [id]
+			};
+
+			client.query(query, (err, res) => {
+			  if (err) {
+			    reject(err.stack);
+			  } else {
+			    resolve(res.rows);
+			  }
+			});
+		});
+	},
+
 	insertOne: (...args) => {
 		return new Promise((resolve, reject) => {
 			const text = `INSERT INTO workouts(
