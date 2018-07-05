bcbf8fbd28f6186ba84e5e23c664bb3e4b8718d6
mattcarpowich1 <mattcarpowich@gmail.com>
2018-07-05 11:42:39 -0700

    mild refactor of route handlers & indentation fixes

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -1,30 +1,26 @@
 const Workouts = require('../models/workout.js');
 
 module.exports = {
-  getWorkouts: req => {
+  getWorkouts: (req, res) => {
     const { id } = req.query;
-    return new Promise((resolve, reject) => {
-       Workouts.findAll(id)
-      .then(results => resolve(results))
-      .catch(err => reject(err));
-    });
+     Workouts.findAll(id)
+    .then(results => res.json(results))
+    .catch(err => console.log(err));
   },
 
-  addWorkout: req => {
-    return new Promise((resolve, reject) => {
-      const {
-        userId,
-        type,
-        distance,
-        duration,
-        comment,
-        feeling,
-        workoutDate
-      } = req.body;
-      
-      Workouts.insertOne(userId, type, distance, duration, comment, feeling, workoutDate)
-      .then(() => resolve())
-      .catch(err => reject(err));
-    });
+  addWorkout: (req, res) => {
+    const {
+      userId,
+      type,
+      distance,
+      duration,
+      comment,
+      feeling,
+      workoutDate
+    } = req.body;
+    
+    Workouts.insertOne(userId, type, distance, duration, comment, feeling, workoutDate)
+    .then(() => res.sendStatus(200))
+    .catch(err => console.log(err));
   }
 }
